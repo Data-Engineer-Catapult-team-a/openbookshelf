e18da02146a5f5ce2989f22eb3d0e3f27ecd333d
SunamotoMikiya <miki38gtr@gmail.com>
2021-10-20 23:51:18 +0900

    マイグレーション作成

---
+++ database/migrations/2021_10_20_143347_create_reviwe_lists_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviweListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviwe_lists', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->string('isbn');
+            $table->integer('evaluation');
+            $table->string('title');
+            $table->string('publisherName');
+            $table->string('mediumImageUrl');
+            $table->string('itemUrl');
+            $table->string('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviwe_lists');
+    }
+}
