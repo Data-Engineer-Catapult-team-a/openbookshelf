2d4c2ca4079b55f2f34d59f44ad2e027fc35c1e0
haruki880 <yaoshengchuankou1@gmail.com>
2021-10-25 23:34:53 +0900

    add view

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,31 +1,29 @@
     <x-app-layout>
   <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <h2 class="font-semibold text-xl  text-gray-800 leading-tight">
       {{ __('検索結果') }}
     </h2>
   </x-slot>
 
-  <div class="py-12">
+  <div class="py-12 ">
     <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
-      <div class="bg-gray-300 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-gray-200 border-b border-gray-200">
-          <table class="text-center w-full border-collapse">
-            <thead>
-              <td> </td>
-                <td class=" bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">検索結果
-                </td>
-            </thead>
+      <div class="overflow-hidden shadow-md sm:rounded-lg ">
+        <div class="p-6 bg-purple-150  border-gray-300">
+          <table class="text-center w-full border-collapse ">
             <tbody>
             @foreach ($result1 as $row)
-              <tr class="hover:bg-grey-lighter">
-                <td class="table-img">
-                <img src="{{$row['mediumImageUrl']}}" alt="商品画像">
+              <tr class="hover:bg-grey-lighter border-b border-grey-light ">
+                <td class="table-img ">
+                  <a href="{{$row['mediumImageUrl']}}">
+                  <img src="{{$row['mediumImageUrl']}}" alt="商品画像"  width="300" height="150">
+                  </a>
                 </td>
-                <td class="py-4 px-6 border-b border-grey-light">
-                  <h3 class="text-left font-bold text-lg text-grey-dark">タイトル： {{$row['title']}}</h3>
+                <td class="py-8 px-6 border-b border-grey-light">
+                  <h2 class="text-left font-bold text-lg text-grey-dark">{{$row['title']}}</h3>
                   <p class="text-left text-grey-dark">出版社： {{$row['publisherName']}}</p>
+                  <br>
                   <a href="{{$row['itemUrl']}}">
-                  <p class="text-left text-grey-dark">URL： {{$row['itemUrl']}}</p>
+                    <p class="hover:text-purple-800 text-left text-grey-dark underline">URL： {{$row['itemUrl']}}</p>
                   </a>
                   <br>
                 </td>
@@ -38,3 +36,4 @@
     </div>
   </div>
 </x-app-layout>
+
