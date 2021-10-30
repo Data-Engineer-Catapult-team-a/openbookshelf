edc6763e5734f3cbdd5392db30ff522d84eefec0
{hisanori-long} <{shimakawa.hisanori@gmail.com}>
2021-10-30 13:23:48 +0900

    ReviewControllerの修正

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -9,12 +9,13 @@ use App\Models\ReviewList;
 class ReviewController extends Controller
 {
     //
-    public function go_personal_page($email)
+    public function go_personal_page(Request $request, int $user_id )
     {
         $reviews =ReviewList::where("user_id", $user_id)->get();
         return view('user.personal_page',[
             "reviews" => $reviews
         ]);
+
     }
 
     public function go_personal_review(Request $request, int $id)
