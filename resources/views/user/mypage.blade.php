e38a1361064062e4330d7630813eec1f991fde6b
SunamotoMikiya <miki38gtr@gmail.com>
2021-10-30 17:13:01 +0900

    マイページのdescription

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -11,24 +11,28 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
           <table class="text-center w-full border-collapse">
-          <thead>
+            <thead>
               <tr>
-                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">{{$user->name}}</th>
-                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">{{$user->user_description}}</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">名前：{{$user->name}}</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">所属・役職：{{$user->user_description}}</th>
               </tr>
             </thead>
-          <tbody>
-              @foreach ($reviews as $review)
-              <tr class="hover:bg-grey-lighter">
-                <button class="text-left font-bold text-lg text-grey-dark">
-                  <td class="py-4 px-6 border-b border-grey-light">
-                    <a href="{{ route('review.go_personal_review', ['id' =>$review->id])}}">{{$review->title}}</a>
-                    <p>★：{{$review->evaluation}}</p>
-                    <img class="" src="{{$review->mediumImageUrl}}"></img>
-                  </td>
-                </button>
-              </tr>
-              @endforeach
+            <tbody>
+          </table>
+          <table class="text-center w-full border-collapse">
+            @foreach ($reviews as $review)
+            <tr class="hover:bg-grey-lighter">
+              <button class="text-left font-bold text-lg text-grey-dark">
+                <td class="py-4 px-6 ">
+                  <img class="" src="{{$review->mediumImageUrl}}"></img>
+                </td>
+                <td>
+                  <a class="hover:text-purple-800 text-grey-dark underline" href="{{ route('review.go_personal_review', ['id' =>$review->id])}}">{{$review->title}}</a>
+                  <p>★：{{$review->evaluation}}</p>
+                </td>
+              </button>
+            </tr>
+            @endforeach
             </tbody>
           </table>
         </div>
