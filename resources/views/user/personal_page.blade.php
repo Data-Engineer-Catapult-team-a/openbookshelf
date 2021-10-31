41b10f324c06a26bb50c04434717ef506fed7232
lll-lll-lll-lll <aistudent80@gmail.com>
2021-10-31 10:35:45 +0900

    personal pageをtailwindに変えた

--- resources/views/user/personal_page.blade.php
+++ resources/views/user/personal_page.blade.php
@@ -13,29 +13,27 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200">
-          <table class="text-center w-full border-collapse">
-            
-            <tbody>
+        <div class="p-6 bg-white border-b border-gray-200 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <!-- <table class="text-center w-full border-collapse"> -->
+            <!-- <tbody> -->
               @foreach ($reviews as $review)
-              <section class="card">
-                <img class="card-img" src="images/bear.jpg" alt="">
-                <div class="card-content">
-                  <img class="" src="{{$review->mediumImageUrl}}"></img>
-                  <a href="{{ route('review.go_personal_review', ['id' =>$review->id])}}">{{$review->title}}</a>
-                  <p>評価：★{{$review->evaluation}}</p>
-                  <!-- <p class="card-text">{{$review->comment}}</p> -->
-                </div>
-                <div class="card-link">
-                  <a href="{{ route('review.go_personal_review', ['id' =>$review->id])}}">Review</a>
-                </div>
-              </section>
+                  <div id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
+                    <img class="card-img" src="{{$review->mediumImageUrl}}"></img>
+                    <h2 class="text-gray-500 text-1xl font-semibold border-b-2 p-3">{{$review->title}}</h2>
+
+                    <div class="mb-10 text-center capitalize p-2" id="whoobe-m2doo">★：{{$review->evaluation}}</div>
+                    <div class="flex justify-end mt-4">
+                      <a class="font-semibold border-b-2 text-xl font-medium text-indigo-500" href="{{ route('review.go_personal_review', ['id' =>$review->id])}}">Review</a>
+                    </div>
+                  </div>
               @endforeach
-            </tbody>
-          </table>
+            <!-- </tbody> -->
+          <!-- </table> -->
         </div>
       </div>
     </div>
+
+  
   </x-app-layout>
 </body>
 </html>
