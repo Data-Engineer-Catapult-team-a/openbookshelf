f3bd97340afac3bb87599cfae54b0c8577d8d4c0
lll-lll-lll-lll <aistudent80@gmail.com>
2021-10-31 10:33:36 +0900

    personal viewをtailwindで整えた

--- resources/views/user/personal_review.blade.php
+++ resources/views/user/personal_review.blade.php
@@ -8,16 +8,17 @@
       {{ __('Personal Review') }}
     </h2>
   </x-slot>
-  
-  <div id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
-    <img class="card-img" src="{{$reviews[0]->mediumImageUrl}}"></img>
-    <h2 class="text-gray-500 text-1xl font-semibold border-b-2 p-3">{{$reviews[0]->title}}</h2>
+  <div class="max-w-md py-4 px-28  bg-white shadow-lg rounded-lg my-2 mx-10 container mx-auto">
+    <div id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
+      <img class="" src="{{$reviews[0]->mediumImageUrl}}"></img>
+      <h2 class="text-gray-500 text-1xl font-semibold border-b-2 p-3">{{$reviews[0]->title}}</h2>
 
-    <div class="mb-10 text-center capitalize p-2" id="whoobe-m2doo">★：{{$reviews[0]->evaluation}}</div>
-    <p>{{$reviews[0]->comment}}</p>
-    
-    <div class="card-link">
-      <a href="{{$reviews[0]->itemUrl}}">楽天SHOP</a>
+      <div class="mb-10 text-center capitalize p-2" id="whoobe-m2doo">★：{{$reviews[0]->evaluation}}</div>
+      <p>{{$reviews[0]->comment}}</p>
+      
+      <div class="card-link">
+        <a href="{{$reviews[0]->itemUrl}}">楽天SHOP</a>
+      </div>
     </div>
   </div>
 
