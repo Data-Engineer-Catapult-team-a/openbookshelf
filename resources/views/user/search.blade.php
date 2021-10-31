5fab799ab06ab236352bb7a5790dfe0741a692a0
SunamotoMikiya <miki38gtr@gmail.com>
2021-10-31 12:26:48 +0900

    ユーザー検索ページのデザイン修正

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -7,31 +7,35 @@
     </h2>
   </x-slot>
 
-  <form class="form-inline my-2 my-lg-0 ml-2">
-      <div class="form-group">
-      <input type="search" class="form-control mr-sm-2" name="search"  value="{{request('find_user')}}" placeholder="キーワードを入力" aria-label="検索...">
-      </div>
-      <input type="submit" value="検索" class="btn btn-info">
-  </form>
-
-
   <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
+    <div class=" max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            <form class="form-inline my-2 my-lg-0 ml-2">
+              <input class="border py-2 px-30 text-grey-darkest" type="search" name="search" id="search" value="{{request('find_user')}}" placeholder="検索したいユーザー名" aria-label="検索">
+              <button type="submit" class="border py-2 px-3 text-grey-darkest  font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
+                検索する
+              </button>
+            </form>
+          </div>
+        </div>
         <div class="p-6 bg-white border-b border-gray-200">
-          <table class="text-center w-full border-collapse">
+          <table class="w-full border-collapse">
             <thead>
               <tr>
-                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">user</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">ユーザー名</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-lg text-grey-dark border-b border-grey-light">所属・役職</th>
               </tr>
             </thead>
             <tbody>
               @foreach ($users as $user)
               <tr class="hover:bg-grey-lighter">
-                <td class="py-4 px-6 border-b border-grey-light">
+                <td class="py-4 px-6">
                   <button class="text-left font-bold text-lg text-grey-dark"><a href="{{ route('review.go_personal_page', ['user_id' =>$user->id])}}">{{$user->name}}</a></button>
-                  <div class="flex">
-                  </div>
+                </td>
+                <td class="hover:bg-grey-lighter">
+                  <button class="text-left font-bold text-lg text-grey-dark"><a href="{{ route('review.go_personal_page', ['user_id' =>$user->id])}}">{{$user->user_description}}</a></button>
                 </td>
               </tr>
               @endforeach
